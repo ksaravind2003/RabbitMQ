7b020cac463c37e7493b9faa97ed4e0ad7e190dc
Aravind <aravind_kalaiselvan_from.tp@mom.gov.sg>
2022-02-25 15:54:35 +0800

    Initial commit

---
+++ Consumer/index.js
@@ -0,0 +1,34 @@
+import amqplib from "amqplib";
+
+const MQ_USER = "guest";
+const MQ_PASSWORD = "guest";
+const MQ_HOST = "localhost";
+const MQ_PORT = "5672";
+const MQ_VIRTUALHOST = "/wins2b";
+const QUEUE_NAME = "to-uam-email-online-queue";
+
+const MQ_DEADLETTEREXCHANGE = "";
+const MQ_DEADLETTERROUTINGKEY = "";
+const MQ_MESSAGETTL = "";
+
+const connection = await amqplib.connect(
+  `amqp://${MQ_USER}:${MQ_PASSWORD}@${MQ_HOST}:${MQ_PORT}/${encodeURIComponent(
+    MQ_VIRTUALHOST
+  )}`
+);
+const channel = await connection.createChannel();
+
+const queueStatus = await channel.assertQueue(QUEUE_NAME, {
+  MQ_DEADLETTEREXCHANGE,
+  MQ_DEADLETTERROUTINGKEY,
+  MQ_MESSAGETTL,
+});
+
+console.log(`Queue status - ${JSON.stringify(queueStatus)}`);
+
+channel.consume(QUEUE_NAME, (message) => {
+  const data = message.content.toString();
+  console.log(JSON.parse(data));
+
+  channel.ack(message);
+});
---
+++ Publisher/index.js
@@ -0,0 +1,38 @@
+import amqplib from "amqplib";
+
+const MQ_USER = "guest";
+const MQ_PASSWORD = "guest";
+const MQ_HOST = "localhost";
+const MQ_PORT = "5672";
+const MQ_VIRTUALHOST = "/wins2b";
+const QUEUE_NAME = "to-uam-email-online-queue";
+
+const MQ_DEADLETTEREXCHANGE = "";
+const MQ_DEADLETTERROUTINGKEY = "";
+const MQ_MESSAGETTL = "";
+
+const message = `{
+    "auth_mechanism": "PLAIN",
+        "channel_max": 2047,
+        "channels": 1
+}`;
+
+const connection = await amqplib.connect(
+  `amqp://${MQ_USER}:${MQ_PASSWORD}@${MQ_HOST}:${MQ_PORT}/${encodeURIComponent(
+    MQ_VIRTUALHOST
+  )}`
+);
+const channel = await connection.createChannel();
+
+const queueStatus = await channel.assertQueue(QUEUE_NAME, {
+  MQ_DEADLETTEREXCHANGE,
+  MQ_DEADLETTERROUTINGKEY,
+  MQ_MESSAGETTL,
+});
+
+console.log(`Queue status - ${JSON.stringify(queueStatus)}`);
+
+channel.sendToQueue(QUEUE_NAME, Buffer.from(JSON.stringify(message)));
+
+await channel.close();
+await connection.close();
